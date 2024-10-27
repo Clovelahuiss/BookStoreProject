940e3efdc525c8e11ff322d864d788656f8600bb
Louis Charpentier <95610858+Clovelahuiss@users.noreply.github.com>
2024-10-27 16:33:38 +0100

    import du bookmodule

--- m1-api/src/app.module.ts
+++ m1-api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// eslint-disable-next-line prettier/prettier
+import { BookModule } from './modules/book/book.module';  
 import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
