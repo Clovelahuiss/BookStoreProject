2d8b5e9f54dc9217839cdecd18d79c5288c60f95
gabincharlemagne <92294664+gabincharlemagne@users.noreply.github.com>
2024-11-07 23:53:52 +0100

    update homepage

--- m1-site/src/app/page.tsx
+++ m1-site/src/app/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import BooksList from '../components/BooksList'; // Assure-toi que le chemin est correct
 
 const HomePage: React.FC = () => {
   return (
     <div>
       <h1>Bienvenue dans la bibliothèque !</h1>
-      <BooksList /> {/* Affiche le composant BooksList ici */}
     </div>
   );
 };
